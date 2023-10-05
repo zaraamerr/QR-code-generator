23e1abe58e8498dabd5d5926795bf03134a953aa
zaraamerr <zaraa.amerr@gmail.com>
2023-10-04 20:55:36 -0400

    basic

---
+++ python-app.py
@@ -0,0 +1,17 @@
+import qrcode
+
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border = 5,
+    )
+
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color = "black", back_color = "white")
+    img.save("qrcodeimg.png")
+
+url = input("Enter a URL: ")
+generate_qrcode(url)
